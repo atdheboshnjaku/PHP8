cbb8fb9c6e7ca3bb446d88148332494c1e3d56b4
Atdhe Boshnjaku <atdhe.boshnjaku@gmail.com>
2021-06-06 14:12:32 +0200

    Updated test24.php

--- test24.php
+++ test24.php
@@ -2,13 +2,16 @@
 ini_set("display_errors", "On");
 
 require_once("test25.php");
+require_once("test26.php");
 
 class ShopProduct
 {
     
     use PriceUtilities;
+    use IdentityTrait;
 
 }
 
 $p = new ShopProduct();
 print $p->calculateTax(100) . "\n";
+print $p->generateID() . "\n";
