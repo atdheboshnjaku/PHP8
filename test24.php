bc801c69563d84631bea447d9c9f7414227eabce
Atdhe Boshnjaku <atdhe.boshnjaku@gmail.com>
2021-06-06 15:04:49 +0200

    Adding test27.php and updating test24.php

--- test24.php
+++ test24.php
@@ -3,15 +3,21 @@ ini_set("display_errors", "On");
 
 require_once("test25.php");
 require_once("test26.php");
+require_once("test27.php");
 
 class ShopProduct
 {
     
     use PriceUtilities;
     use IdentityTrait;
+    use TaxTools {
+        TaxTools::calculateTax insteadof PriceUtilities;
+        PriceUtilities::calculateTax as basicTax;
+    }
 
 }
 
 $p = new ShopProduct();
 print $p->calculateTax(100) . "\n";
 print $p->generateID() . "\n";
+print $p->basicTax(100) . "\n";
---
+++ test27.php
@@ -0,0 +1,12 @@
+<?php 
+ini_set("display_errors", "On");
+
+trait TaxTools
+{
+
+    public function calculateTax(float $price): float 
+    {
+        return 222;
+    }
+
+}
