bb4e8ae519b37e22bc4c0488b0df7a49d0205709
Atdhe Boshnjaku <atdhe.boshnjaku@gmail.com>
2021-06-07 20:16:28 +0200

    Adding t39.php

---
+++ test39.php
@@ -0,0 +1,35 @@
+<?php 
+ini_set("display_errors", "On");
+
+class Person
+{
+
+    public function __get(string $property): mixed 
+    {
+        $method = "get{$property}";
+        if(method_exists($this, $method)) {
+            return $this->$method();
+        }
+    }
+
+    public function getName(): string 
+    {
+        return "Bob";
+    }
+
+    public function getAge(): int 
+    {
+        return 44;
+    }
+
+}
+
+$p = new Person();
+print $p->name;
+
+
+
+
+
+
+
