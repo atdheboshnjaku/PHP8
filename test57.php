01b05736aafbd9dec761d7b201b685229667d019
Atdhe Boshnjaku <atdhe.boshnjaku@gmail.com>
2021-06-13 23:59:24 +0200

    Adding t57

---
+++ test57.php
@@ -0,0 +1,50 @@
+<?php 
+ini_set("display_errors", "On");
+
+require_once("test51.php");
+require_once("test55.php");
+
+class ProcessSale
+{
+
+    private array $callbacks;
+
+    public function registerCallback(callable $callback): void 
+    {
+        $this->callbacks[] = $callback;
+    }
+
+    public function sale(Product $product): void 
+    {
+        print "{$product->name}: processing \n";
+        foreach($this->callbacks as $callback) {
+            call_user_func($callback, $product);
+        }
+    }
+
+}
+
+$processor = new ProcessSale();
+
+$processor->registerCallback(Totalizer::warnAmount());
+
+$processor->sale(new Product("shoes", 6));
+print "\n";
+$processor->sale(new Product("coffee", 6));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
