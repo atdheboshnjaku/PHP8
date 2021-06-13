b278989c0827384bd3b1b3f7a75259cc0c9e9690
Atdhe Boshnjaku <atdhe.boshnjaku@gmail.com>
2021-06-13 23:57:53 +0200

    Adding t56

---
+++ test56.php
@@ -0,0 +1,18 @@
+<?php 
+ini_set("display_errors", "On");
+
+require_once("test51.php");
+
+class Totalizer
+{
+
+    public static function warnAmount(): callable 
+    {
+        return function (Product $product) {
+            if($product->price > 5) {
+                print "  reached high price: {$product->price}\n";
+            }
+        };
+    }
+
+}
