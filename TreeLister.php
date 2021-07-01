c1d3246e4887a1477fe1f51582a66a1b77fde00b
Atdhe Boshnjaku <atdhe.boshnjaku@gmail.com>
2021-07-01 12:58:24 +0200

    Adding treelister and ch05 folder

---
+++ TreeLister.php
@@ -0,0 +1,11 @@
+<?php
+
+class TreeLister
+{
+
+    public static function helloWorld(): void 
+    {
+        print "Hello from root namespace\n";
+    }
+
+}
---
+++ ch05/Blah.php
@@ -0,0 +1,11 @@
+<?php
+
+class Blah
+{
+
+    public function wave(): void 
+    {
+        print "saying hi from root";
+    }
+
+}
